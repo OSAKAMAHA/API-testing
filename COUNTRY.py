d0bea2cd44228ded6cf78e48869d1d58a94286cd
Disoky <seif.mohamed999@yahoo.com>
2020-04-23 22:13:56 +0200

    assignment2

--- COUNTRY.py
+++ COUNTRY.py
@@ -2,9 +2,11 @@ from flask import Flask,request
 from flask import jsonify
 import requests
 import json  
+import redis
  
 app = Flask(__name__) 
 
+redis = redis.Redis()
 
 def connect(name):
     url = "https://restcountries.eu/rest/v2/name/"
@@ -13,7 +15,6 @@ def connect(name):
     return c
 
 
-@app.route("/<string:name>")
 def country_info(name: str = None):
     x = connect(name)
     response = requests.get(x).content
@@ -21,29 +22,54 @@ def country_info(name: str = None):
     if response is None:
         return "Error in Connection"
     else:
-        return response
-    
+         redis.setex (name,60,str(response))
+         
+         
+@app.route("/<string:name>")    
+def fetch_cache_name(name: str = None):
+    result = redis.get(name)
+    if not result:
+        country_info(name)
+        result = redis.get(name)
+        return str(result) + "new"
+    else:
+        return result
+
 
 
 @app.route("/<string:name>/<info>")  
 def Info(name: str = None,info = None): 
-      
-    country = country_info(name)
-    info = info.split(",")
-    out = "{"
-    
-    websiteinfo = json.loads(country)[0]
-    if websiteinfo is None:
-        return "error in name"
-    else:  
-        for s in info:
-            try:
-                out += s+":"+str(websiteinfo[s])+ ","
-            except KeyError:
-                return "wrong info"
-        out += "}"
+    x = connect(name)
+    response = requests.get(x).content
+    country = response
+  
+    result = redis.get(str(info))
+    if not result:
 
-        return out
+        info = info.split(",")
+        out = "{"
+        websiteinfo = json.loads(country)[0]
+        if websiteinfo is None:
+            return "error in name"
+        else:  
+            for s in info:
+                try:
+                    out += s+":"+str(websiteinfo[s])+ ","
+                except KeyError:
+                    return "wrong info"
+            out += "}"
+            redis.setex (str(info),60,str(out))
+            result = redis.get(str(info))
+            return str(result) 
+    else:
+        return result      
     
+
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True, port=8080) #run app on port 8080 in debug mode
+    
+    
+    
